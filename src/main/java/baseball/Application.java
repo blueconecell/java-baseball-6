b33be64bfeac509ca3dad2cd944ed86e3e39412f
blueconecell <95961799+blueconecell@users.noreply.github.com>
2024-10-11 18:06:00 +0900

    feature : 기능 완성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,42 +11,81 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         // 인트로
-        System.out.println("숫자 야구 게임을 시작합니다.\n");
+        intro();
+        String gameContinue = "1";
 
-        // 야구 숫자 랜덤 세팅
-        List<Integer> computer = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        while (gameContinue == "1") {
+            // 야구 숫자 랜덤 세팅
+            List<Integer> computer = getComputerNumber();
+            System.out.println("computer = " + computer);
 
-        // 입력 무한히 받아보기
-        System.out.print("숫자를 입력해주세요 : ");
-        String inn = Console.readLine();
-        String[] strArr = inn.split("");
-        int[] input = new int[3];
-        for (int i = 0; i < 3; i++) {
-            input[i] = Integer.parseInt(strArr[i]);
-        }
-        System.out.println(input);
 
-        int [] checker = new int[2];
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (computer.get(j) == input[i]) {
-                    if (i == j) {
-                        checker[0]++;
-                        break;
-                    } else {
-                        checker[1]++;
-                        break;
-                    }
-                }
+            // 입력 무한히 받아보기
+            boolean gameResult = false;
+            while (!gameResult) {
+                gameResult = playMainGame(computer);
             }
+
+            // 게임 종료 & 재시작 여부
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            gameContinue = Console.readLine();
         }
-        System.out.println("computer = " + computer);
-        displayArray(checker,"checker");
+
+
 
     }
 
+    private static List<Integer> getComputerNumber() {
+        List<Integer> computer = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        return computer;
+    }
+
+    private static void intro() {
+        System.out.println("숫자 야구 게임을 시작합니다.\n");
+    }
+
+    private static boolean playMainGame(List<Integer> computer) {
+        System.out.print("숫자를 입력해주세요 : ");
+        String inn = Console.readLine();
+
+        String[] strArr = inn.split("");
+        int[] inputNum = new int[3];
+        for (int i = 0; i < 3; i++) {
+            inputNum[i] = Integer.parseInt(strArr[i]);
+        }
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i < 3; i++) {
+            if (computer.get(i) == inputNum[i]) {
+                strike++;
+            } else if (computer.contains(inputNum[i])) {
+                ball++;
+            }
+        }
+
+        if (ball > 0) {
+            System.out.print(ball+"볼 ");
+            if (strike > 0) {
+                System.out.println(strike + "스트라이크");
+            }
+        } else if (strike > 0) {
+            System.out.println(strike + "스트라이크");
+        } else {
+            System.out.println("낫싱");
+        }
+        if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }return false;
+    }
+
     private static void displayArray(int[] checker,String name) {
+        for (int i = 0; i < checker.length; i++) {
+            System.out.println(name + "["+i+"] : "+checker[i]);
+
+        }
+    }
+    private static void displayArray(String[] checker,String name) {
         for (int i = 0; i < checker.length; i++) {
             System.out.println(name + "["+i+"] : "+checker[i]);
         }
